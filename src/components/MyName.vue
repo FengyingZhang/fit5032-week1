950dcff9517cc75ecf7327752e04f8569d8ac630
fzha7022 <fzha7022@uni.sydney.edu.au>
2024-07-27 14:40:05 +0800

    ADD NAME

---
+++ src/components/MyName.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="greetings">
+      <h1 class="green">Fengying Zhang</h1>
+      <h3>
+        <a href="https://github.com/FengyingZhang/fit5032-week1" target="_blank" rel="noopener">My Github</a>
+      </h3>
+    </div>
+  </template>
+
+<style scoped>
+h1 {
+  font-weight: 500;
+  font-size: 2.6rem;
+  position: relative;
+  top: -10px;
+}
+
+h3 {
+  font-size: 1.2rem;
+}
+
+.greetings h1,
+.greetings h3 {
+  text-align: center;
+}
+
+@media (min-width: 1024px) {
+  .greetings h1,
+  .greetings h3 {
+    text-align: left;
+  }
+}
+</style>
